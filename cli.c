d54850a4d9e1f727c1d6fb367cd8f8ddad1ccfcb
Gautam Sharma <95131287+g-s01@users.noreply.github.com>
2023-06-26 11:38:34 +0530

    edge cases fixed

--- cli.c
+++ cli.c
@@ -19,8 +19,8 @@ typedef struct
 typedef struct 
 {
 	uint32_t id;
-	char username[COLUMN_USERNAME_SIZE];
-	char email[COLUMN_EMAIL_SIZE];
+	char username[COLUMN_USERNAME_SIZE+1];
+	char email[COLUMN_EMAIL_SIZE+1];
 }row;
 
 typedef enum
@@ -32,6 +32,8 @@ typedef enum
 typedef enum
 {
 	PREPARE_SUCCESS,
+	PREPARE_NEGATIVE_ID,
+	PREPARE_STRING_TOO_LONG,
 	PREPARE_SYNTAX_ERROR, 
 	PREPARE_UNRECOGNIZED_STATEMENT
 }prepareResult;
@@ -147,16 +149,32 @@ metaCommandResult do_meta_command(inputBuffer * input_buffer, table * t)
 	}
 }
 
+prepareResult prepare_insert(inputBuffer * input_buffer, statement * exp)
+{
+	exp->type = STATEMENT_INSERT;
+
+	char * keyword = strtok(input_buffer->buffer, " ");
+	char * id_string = strtok(NULL, " ");
+	char * username = strtok(NULL, " ");
+	char * email = strtok(NULL, " ");
+	
+	if(id_string == NULL || username == NULL || email == NULL) return PREPARE_SYNTAX_ERROR;
+	
+	int id = atoi(id_string);
+	if(id < 0) return PREPARE_NEGATIVE_ID;
+	if(strlen(username) > COLUMN_USERNAME_SIZE)	return PREPARE_STRING_TOO_LONG;
+	if(strlen(email) > COLUMN_EMAIL_SIZE) return PREPARE_STRING_TOO_LONG;
+	
+	exp->row_to_insert.id = id;
+	strcpy(exp->row_to_insert.username, username);	
+	strcpy(exp->row_to_insert.email, email);
+	
+	return PREPARE_SUCCESS;
+}
+
 prepareResult prepare_statement(inputBuffer* input_buffer, statement * exp)
 {
-	if(strncmp(input_buffer->buffer, "insert", 6) == 0)
-	{
-		exp->type = STATEMENT_INSERT;
-		int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(exp->row_to_insert.id),
-		exp->row_to_insert.username, exp->row_to_insert.email);
-		if(args_assigned < 3) return PREPARE_SYNTAX_ERROR;
-		return PREPARE_SUCCESS;
-	}
+	if(strncmp(input_buffer->buffer, "insert", 6) == 0) return prepare_insert(input_buffer, exp);
 	else if(strcmp(input_buffer->buffer, "select") == 0)
 	{
 		exp->type = STATEMENT_SELECT;
@@ -239,6 +257,11 @@ int main()
 		{
 			case (PREPARE_SUCCESS):
 				break;
+			case (PREPARE_NEGATIVE_ID):
+				printf("ID must be positive.\n");
+				continue;	
+			case (PREPARE_STRING_TOO_LONG):
+				printf("String is too long.\n");
 			case (PREPARE_SYNTAX_ERROR):
 				printf("Syntax error. Could not parse statement.\n");
 				continue;
